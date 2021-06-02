97e4b65a54762ace500e368cee37381e0b7e82cb
Samet Talayhan <samet.talayhan@gmail.com>
2021-06-02 13:50:31 +0300

    Add logs to http server

--- http-server/https.js
+++ http-server/https.js
@@ -2,9 +2,12 @@
 var http = require("http");
 
 function onRequest(request, response) {
+	console.log("Request received!")
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World!");
 	response.end();
 }
 
 http.createServer(onRequest).listen(9292);
+
+console.log("Server has started!");

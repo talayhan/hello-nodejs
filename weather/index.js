b56510e112800fdc83a10efa9364d0693d19a207
Samet Talayhan <samet.talayhan@gmail.com>
2021-06-03 22:24:18 +0300

    [weather] Parse json

--- weather/index.js
+++ weather/index.js
@@ -2,12 +2,15 @@ const request = require('request');
 
 let apiKey = '?';
 let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url =
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 request(url, function(err, response, body) {
-    if (err) {
-	console.log('error:', error);
-    } else {
-	console.log('body:', body);
+	if (err) {
+		console.log('error:', error);
+	} else {
+		let weather = JSON.parse(body)
+		let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+		console.log(message);
     }
 });
